a7a26c6e5022367ac79876693a83c19aff8e2ec9
n-sndr <ino311@gmail.com>
2022-09-30 15:09:48 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,11 @@
 terraform {
+  required_version = ">= 1.1.0"
   required_providers {
     azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0.2"
+      source = "hashicorp/azurerm"
+      version = "3.25.0"
     }
   }
-
-  required_version = ">= 1.1.0"
 }
 
 provider "azurerm" {
