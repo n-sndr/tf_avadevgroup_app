0e5240f95579c107f6474ef24c9dd7543e49911e
n-sndr <ino311@gmail.com>
2022-10-01 12:49:48 -0700

    f

--- versions.tf
+++ versions.tf
@@ -1,11 +1,5 @@
 terraform {
   required_version = ">= 1.1.0"
-  required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-      version = "3.25.0"
-    }
-  }
   cloud {
     organization = "avadevgroup"
     workspaces {
