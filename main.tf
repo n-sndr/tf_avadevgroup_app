adc9bfa18c47e06733d180b977a445552928f606
n-sndr <ino311@gmail.com>
2022-10-01 11:13:12 -0700

    update terraform location

--- main.tf
+++ main.tf
@@ -1,3 +1,26 @@
+terraform {
+  required_version = ">= 1.1.0"
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.25.0"
+    }
+  }  
+  cloud {
+    organization = "avadevgroup"
+    workspaces {
+      name = "tf_avadevgroup_app_nonprd"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = var.subscriptionId
+  tenant_id = var.tenantId
+  client_id = var.clientId
+}
+
 module "webportal-dev" {
   source = "./modules/dev"
 }
--- modules/dev/main.tf
+++ modules/dev/main.tf
@@ -1,25 +1,3 @@
-terraform {
-  required_version = ">= 1.1.0"
-  required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-      version = "3.25.0"
-    }
-  }  
-  cloud {
-    organization = "avadevgroup"
-    workspaces {
-      name = "tf_avadevgroup_app_nonprd"
-    }
-  }
-}
-provider "azurerm" {
-  features {}
-  subscription_id = var.subscriptionId
-  tenant_id = var.tenantId
-  client_id = var.clientId
-}
-
 resource "azurerm_resource_group" "dev" {
   name     = var.RGName
   location = var.location
