783cf1a69878a9f51a5fdc3a740e2797c7943d51
n-sndr <ino311@gmail.com>
2022-10-01 12:48:46 -0700

    e

--- main.tf
+++ main.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_version = ">= 1.1.0"
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.25.0"
+    }
+  }
+}
+
 provider "azurerm" {
   features {}
 }
