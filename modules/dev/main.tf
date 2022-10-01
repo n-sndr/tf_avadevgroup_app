397f1bacfbd3cacdea0afb91ba9c0060a80a2798
n-sndr <ino311@gmail.com>
2022-10-01 13:02:41 -0700

    i

--- modules/dev/main.tf
+++ modules/dev/main.tf
@@ -10,17 +10,17 @@ resource "azurerm_resource_group" "dev" {
 
 resource "azurerm_service_plan" "dev" {
   name                = "ava-dev-group-asp"
-  resource_group_name = azurerm_resource_group.dev.name
-  location            = azurerm_resource_group.dev.location
+  resource_group_name = module.azurerm_resource_group.dev.name
+  location            = module.azurerm_resource_group.dev.location
   os_type             = "Windows"
   sku_name            = "D1"
 }
 
 resource "azurerm_windows_web_app" "dev" {
   name                = "ava-dev-group-web001"
-  resource_group_name = azurerm_resource_group.dev.name
-  location            = azurerm_service_plan.dev.location
-  service_plan_id     = azurerm_service_plan.dev.id
+  resource_group_name = module.azurerm_resource_group.dev.name
+  location            = module.azurerm_service_plan.dev.location
+  service_plan_id     = module.azurerm_service_plan.dev.id
 
   site_config {
     always_on         = false 
